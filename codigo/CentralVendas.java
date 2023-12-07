b2a4ed508b590444364a0a603cb4d21fbd408a29
PHPGomes <pedrohpgomes@gmail.com>
2023-12-07 10:00:21 -0300

    .

--- codigo/CentralVendas.java
+++ codigo/CentralVendas.java
@@ -5,10 +5,8 @@ import java.util.Scanner;
 public class CentralVendas {
     private RelacaoFuncionarios relacaoFuncionarios;
     private Estoque estoque;
-    private Scanner leitura;
 
     public CentralVendas() {
-        leitura = new Scanner(System.in);
         relacaoFuncionarios = new RelacaoFuncionarios();
         estoque = Estoque.getIntance();
     }
@@ -43,23 +41,6 @@ public class CentralVendas {
         return relacaoFuncionarios.getProdutoPosFuncionario(pFuncionario, pProduto);
     }
 
-    /*
-     * if(escolha.equals("2")){
-     * String nome;
-     * int codigo;
-     * System.out.println("Digite o nome do funcionario que realizou a venda");
-     * nome = leitura.nextLine();
-     * while(!relacaoFuncionarios.tem(nome)){
-     * System.out.println("Funcionario nao encontrado");
-     * System.out.
-     * println("Digite o nome correto do funcionario que realizou a venda");
-     * nome = leitura.nextLine();
-     * }
-     * System.out.println("Digite o codigo do produto");
-     * codigo = Integer.parseInt(leitura.nextLine());
-     * realizarVenda(codigo, buscarFuncionario(nome));
-     * }
-     */
 
     public boolean tem(String nome){
         return relacaoFuncionarios.tem(nome);
@@ -69,18 +50,14 @@ public class CentralVendas {
         return relacaoFuncionarios.buscarFuncionario(nome);
     }
 
-    private void exibirEstoque() {
-        System.out.println("\nEstoque Disponivel:");
-        System.out.println(estoque);
-    }
-
+    
     public void realizarVenda(int codigo, String nome){
         associarVenda(codigo, buscarFuncionario(nome));
     }
 
     private void associarVenda(int codigo, Funcionario f) {
-        f.adiconarVenda(estoque.getSapato(codigo));// passar o sapato para o vendedor
-        estoque.removerSapato(codigo);// tirar do estoque
+        f.adiconarVenda(estoque.getSapato(codigo));
+        estoque.removerSapato(codigo);
     }
 
 }
